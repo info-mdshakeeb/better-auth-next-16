4ecfdce1db7f1fa1bbb8e2b68dc1888e12c1a222
m3-shakeeb <shakeeb.m360ict@gmail.com>
2025-11-10 18:44:50 +0600

    [added] : sonner

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,40 +1,49 @@
-"use client"
+"use client";
 
 import {
   CircleCheckIcon,
   InfoIcon,
-  Loader2Icon,
+  Loader2,
   OctagonXIcon,
   TriangleAlertIcon,
-} from "lucide-react"
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, type ToasterProps } from "sonner"
+} from "lucide-react";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, type ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      toastOptions={{
+        style: {
+          maxWidth: "32rem",
+          marginInline: "auto",
+          // width: "fit-content",
+          // padding: "7px 14px",
+        },
+      }}
       icons={{
         success: <CircleCheckIcon className="size-4" />,
         info: <InfoIcon className="size-4" />,
         warning: <TriangleAlertIcon className="size-4" />,
         error: <OctagonXIcon className="size-4" />,
-        loading: <Loader2Icon className="size-4 animate-spin" />,
+        loading: <Loader2 className="size-4 animate-spin" />,
       }}
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-          "--border-radius": "var(--radius)",
-        } as React.CSSProperties
-      }
+      // style={
+      //   {
+      //     padding: "0",
+      //     "--normal-bg": "var(--popover)",
+      //     "--normal-text": "var(--popover-foreground)",
+      //     "--normal-border": "var(--border)",
+      //     "--border-radius": "var(--radius)",
+      //   } as React.CSSProperties
+      // }
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
